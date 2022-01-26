418253aee8bec880dc94180ea7165f181da96d0b
ilobosok <ilobosoki@gmail.com>
2022-01-26 18:43:08 +0300

    error solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,155 @@
+import 'dart:async';
+import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:music/ui/MainPage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'database/score.dart';
+import 'model/Product.dart';
+import 'model/User.dart';
+import 'services/Authenticate.dart';
+import 'services/helper.dart';
+import 'constants.dart' as Constants;
 
-
-void main() {
-  runApp(MyApp());
+void main(){
+  runApp(
+      ChangeNotifierProvider<Score>(
+        create: (_) => new Score(),
+        child: MyApp(),
+      )
+  );
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  static User currentUser;
+  static Product currentProduct;
+  StreamSubscription tokenStream;
+
+  // Set default `_initialized` and `_error` state to false
+  bool _initialized = false;
+  bool _error = false;
+
+  // Define an async function to initialize FlutterFire
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (_error) {
+      return MaterialApp(
+          home: Scaffold(
+            body: Container(
+              color: Colors.white,
+              child: Center(
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.error_outline,
+                        color: Colors.red,
+                        size: 25,
+                      ),
+                      SizedBox(height: 16),
+                      Text(
+                        'Failed to initialise firebase!',
+                        style: TextStyle(color: Colors.red, fontSize: 25),
+                      ),
+                    ],
+                  )),
+            ),
+          ));
+    }
+
+    // Show a loader until FlutterFire is initialized
+    if (!_initialized) {
+      return Container(
+        color: Colors.white,
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainPage(),
+        theme: ThemeData(accentColor: Color(Constants.COLOR_PRIMARY)),
+        debugShowCheckedModeBanner: false,
+        color: Color(Constants.COLOR_PRIMARY),
+        home: MainPage());
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (auth.FirebaseAuth.instance.currentUser != null && currentUser != null) {
+      if (state == AppLifecycleState.paused) {
+        //user offline
+        tokenStream.pause();
+
+        currentUser.lastOnlineTimestamp = Timestamp.now();
+        FireStoreUtils.updateCurrentUser(currentUser);
+      } else if (state == AppLifecycleState.resumed) {
+        //user online
+        tokenStream.resume();
+
+        FireStoreUtils.updateCurrentUser(currentUser);
+      }
+    }
+  }
+}
+
+class OnBoarding extends StatefulWidget {
+  @override
+  State createState() {
+    return OnBoardingState();
+  }
+}
+
+class OnBoardingState extends State<OnBoarding> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Center(
+        child: CircularProgressIndicator(
+          backgroundColor: Colors.white,
+        ),
+      ),
     );
   }
 }
